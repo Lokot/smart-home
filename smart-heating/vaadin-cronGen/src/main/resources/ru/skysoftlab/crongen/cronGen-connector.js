0920e965d218d8b833a7640f6b493cc43d2b25bc
Lokot <loktionovag@gmail.com>
2016-04-01 00:09:05 +0300

    edit connector

--- smart-heating/vaadin-cronGen/src/main/resources/ru/skysoftlab/crongen/cronGen-connector.js
+++ smart-heating/vaadin-cronGen/src/main/resources/ru/skysoftlab/crongen/cronGen-connector.js
@@ -2,18 +2,24 @@ window.ru_skysoftlab_crongen_CronGenField = function() {
 	var e = this.getElement();
 	var domId = this.getState().domId;
 	
+	e.innerHTML = '<input id="'
+		+ domId
+		+ '_input" class="v-textfield v-widget v-textfield-required v-required v-has-width" />';
+	$('#' + domId + '_input').cronGen(domId);
+	
+	var connector = this;
+	$('#' + domId + '_input').change(function() {
+		connector.getState().value = $('#' + domId + '_input').val();
+//		e.value
+	});
+
 	// Getter and setter for the value property
-	this.getValue = function () {
-		return e.value;
-	};
-	
-	this.setValue = function (value) {
-		e.value = value;
-	};
-	
+//	this.setValue = function() {
+//		return this.getState().value;
+//	};
+
 	this.onStateChange = function() {
-		e.innerHTML = '<input id="'+domId+'_input" class="v-textfield v-widget v-textfield-required v-required v-has-width" />';
-		$('#' + domId + '_input').cronGen();
+		$('#' + domId + '_input').val(this.getState().value);
 	}
-	
+
 };
